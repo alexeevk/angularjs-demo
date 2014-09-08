1083153d018b99b27bccdad15eed4a47789fb972
Konstantin Alexeyev (laptop) <konstantin.alexeyev@activenetwork.com>
2014-09-08 00:51:50 -0700

    Filtering. Camel filter example

---
+++ AngularFromScratch/assets/filters/camelFilter.js
@@ -0,0 +1,14 @@
+﻿myApp.filter("camel", function () {
+    return function (input) {
+        var out = "";
+        var isUpper = true;
+        if (input != null && input.length > 0) {
+            for (var i = 0; i < input.length; i++) {
+                out = out + (isUpper ? input[i].toUpperCase() : input[i].toLowerCase());
+                isUpper = input[i] == ' ';
+            }
+        }
+
+        return out;
+    }
+});
