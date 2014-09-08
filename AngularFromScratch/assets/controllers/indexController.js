0a3e3f86603a3acf0820273aa3ae67f6baac23bf
Konstantin Alexeyev (laptop) <konstantin.alexeyev@activenetwork.com>
2014-09-07 23:55:40 -0700

    List of posts retrieved by button

--- AngularFromScratch/assets/controllers/indexController.js
+++ AngularFromScratch/assets/controllers/indexController.js
@@ -1,4 +1,19 @@
-﻿myApp.controller("indexController", function () {
+﻿myApp.controller("indexController", function (dataService) {
     this.person = { name: "Adam Smith" };
     this.book = { title: "The Theory of Moral Sentiments" };
+
+    this.posts = [];
+
+    this.getPosts = function () {
+        var self = this;
+        dataService.getPosts()
+            .then(
+                function (data) {
+                    self.posts = data;
+                },
+                function (error) {
+
+                });
+    }
+
 })
---
+++ AngularFromScratch/assets/services/dataService.js
@@ -0,0 +1,32 @@
+﻿myApp.service("dataService", function ($http, $q) {
+
+    // http://jsonplaceholder.typicode.com/posts
+    // http://jsonplaceholder.typicode.com/photos
+    var createRequest = function (url) {
+        return {
+            url: url,
+            method: "GET",
+            "Content-Type": "application/json; charset=utf-8",
+            "X-Requested-With": "XMLHttpRequest"
+        }
+    };
+
+    return {
+        getPosts: function () {
+            var deferred = $q.defer();
+            var getPosts = createRequest("http://jsonplaceholder.typicode.com/posts");
+
+            $http(getPosts)
+                .success(
+                    function (response) {
+                        deferred.resolve(response);
+                    })
+                .error(
+                    function (error, statusCode) {
+                        deferred.reject(error);
+                    });
+
+            return deferred.promise;
+        }
+    }
+})
