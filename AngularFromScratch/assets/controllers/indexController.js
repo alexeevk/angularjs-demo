557be412e3b9e1086ce3661cf1870c9d1fbfdbed
Konstantin Alexeyev (laptop) <konstantin.alexeyev@activenetwork.com>
2014-09-08 01:28:56 -0700

    What is $scope. Add WATCHER.

--- AngularFromScratch/assets/controllers/indexController.js
+++ AngularFromScratch/assets/controllers/indexController.js
@@ -1,8 +1,18 @@
-﻿myApp.controller("indexController", function (dataService) {
+﻿myApp.controller("indexController", function (dataService, $scope) {
     this.person = { name: "Adam Smith" };
     this.book = { title: "The Theory of Moral Sentiments" };
 
+    $scope.$watch("model.person.name", function (newValue, oldValue) {
+        if (newValue != null && newValue.toLowerCase() == "konstantin alexeyev") {
+            $scope.myCss = "thePerson";
+            return;
+        }
+
+        $scope.myCss = null;
+    });
+
     this.posts = [];
+
 
     this.getPosts = function () {
         var self = this;
