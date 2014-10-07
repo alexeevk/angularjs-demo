99aedba27f203d2c453c519303bfae1a3cc03543
Konstantin Alexeyev (laptop) <konstantin.alexeyev@activenetwork.com>
2014-10-07 10:53:05 -0700

    Modifying DOM. timerDirective.js

--- AngularFromScratch/assets/controllers/postsController.js
+++ AngularFromScratch/assets/controllers/postsController.js
@@ -1,6 +1,7 @@
 ﻿myApp.controller("postsController", function (dataService) {
     this.posts = [];
     this.controllersScopeMessage = 'this is custom post message';
+    this.format = 'M/d/yy h:mm:ss a';
     this.getPosts = function () {
         var self = this;
         dataService.getPosts()
---
+++ AngularFromScratch/assets/directives/timerDirective.js
@@ -0,0 +1,33 @@
+﻿myApp.directive('currentTime', ['$interval', 'dateFilter', function ($interval, dateFilter) {
+
+    // scope - angular scope service
+    // element - directive's HTML element (to whatever it matches)
+    // attrs - list of attributes applied to the directive
+    function link(scope, element, attrs) {
+        var format,
+            timeoutId;
+
+        function updateTime() {
+            element.text(dateFilter(new Date(), format));
+        }
+
+        scope.$watch(attrs.format, function (value) {
+            format = value;
+            updateTime();
+        });
+
+        element.on('$destroy', function () {
+            $interval.cancel(timeoutId);
+        });
+
+        // start the UI update process; save the timeoutId for canceling
+        timeoutId = $interval(function () {
+            updateTime(); // update DOM
+        }, 1000);
+    }
+
+    return {
+        link: link,
+        restrict: 'E'
+    };
+}]);
