dd52bacbc948eb624c48423319d5b6f4d06d66b8
Konstantin Alexeyev (laptop) <konstantin.alexeyev@activenetwork.com>
2014-10-07 11:33:04 -0700

    Add comments

--- AngularFromScratch/assets/directives/timerDirective.js
+++ AngularFromScratch/assets/directives/timerDirective.js
@@ -16,6 +16,9 @@
             updateTime();
         });
 
+        // listen for $destroy that is sent when scope is destroyed
+        // listeners registred on elements are destoyed automatically, but
+        // if we do custom listeners on services we have to destroy it explicitly
         element.on('$destroy', function () {
             $interval.cancel(timeoutId);
         });
