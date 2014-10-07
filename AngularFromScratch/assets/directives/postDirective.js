8061b4bf215c8f1f29e25a1279680ceec2fc214a
Konstantin Alexeyev (laptop) <konstantin.alexeyev@activenetwork.com>
2014-10-07 09:31:00 -0700

    Isolate directive scope

--- AngularFromScratch/assets/directives/postDirective.js
+++ AngularFromScratch/assets/directives/postDirective.js
@@ -1,5 +1,14 @@
 ﻿myApp.directive('blogPost', function() {
     return {
+        // Possible restrictions are
+        // E - matches html element
+        // A - matches attribute name
+        // C - matches class name
+        // combinations are possible 
+        restrict: 'E',
+        scope: {
+            blogItem: '=posting'  // syntax means that posting is an attribute
+        },
         templateUrl: '/assets/views/post.html'
     };
 });
